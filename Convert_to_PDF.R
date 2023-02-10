d85155c9bdcdf9f1647fa4317dd5fb666445eb5f
HuguesPecout <hugues.pecout@cnrs.fr>
2023-02-10 18:52:33 +0100

    update

--- Convert_to_PDF.R
+++ Convert_to_PDF.R
@@ -1,34 +1,47 @@
-library(quarto)
 
-pth <- getwd()
-
-quarto_render(paste0(pth,"/modules/import/import.qmd"), output_file = "import.pdf", output_format = "pdf")
-# quarto_render(paste0(pth,"/modules/graphique/Rbase_graph.qmd"), output_file = "Rbase_graph.pdf", output_format = "pdf")
-quarto_render(paste0(pth,"/modules/reproductibilite/minicran.qmd"), output_file = "miniCRAN.pdf", output_format = "pdf")
-
-
-
-
+# IMPORT
 file <- "_site/modules/import"
 list_files <- list.files(file, ".html")
-file.copy( from = paste0(file,list_files)
-           
-           
-           > to = "/Modules_R", list_files, "/Modules_R")
+file.copy(from = file.path(file,list_files), to = paste0("Modules_R/", list_files), overwrite = TRUE)
+       
+# MANIPULATION
+file <- "_site/modules/manipulation"
+list_files <- list.files(file, ".html")
+file.copy(from = file.path(file,list_files), to = paste0("Modules_R/", list_files), overwrite = TRUE)
+
+# STATISTIQUE
+file <- "_site/modules/statistique"
+list_files <- list.files(file, ".html")
+file.copy(from = file.path(file,list_files), to = paste0("Modules_R/", list_files), overwrite = TRUE)
+
+# GRAPHIQUE
+file <- "_site/modules/graphique"
+list_files <- list.files(file, ".html")
+file.copy(from = file.path(file,list_files), to = paste0("Modules_R/", list_files), overwrite = TRUE)
+
+# GEOMATIQUE
+file <- "_site/modules/geomatique"
+list_files <- list.files(file, ".html")
+file.copy(from = file.path(file,list_files), to = paste0("Modules_R/", list_files), overwrite = TRUE)
+
+# CARTOGRAPHIE
+file <- "_site/modules/cartographie"
+list_files <- list.files(file, ".html")
+file.copy(from = file.path(file,list_files), to = paste0("Modules_R/", list_files), overwrite = TRUE)
+
+# REPRODUCTIBILITE
+file <- "_site/modules/reproductibilite"
+list_files <- list.files(file, ".html")
+file.copy(from = file.path(file,list_files), to = paste0("Modules_R/", list_files), overwrite = TRUE)
+
+# CSS
+file <- "./"
+list_files <- list.files(file, ".css")
+file.copy(from = file.path(file,list_files), to = paste0("Modules_R/", list_files), overwrite = TRUE)
 
 
-manip <- "_site/modules/manipulation"
-list_files   <- c(list_files, list.files(manip, ".html"))
-stat <- "_site/modules/statistique"
-list_files   <- c(list_files, list.files(stat, ".html"))
-graph <- "_site/modules/graphique"
-list_files   <- c(list_files, list.files(graph, ".html"))
-geo <- "_site/modules/geomatique"
-list_files   <- c(list_files, list.files(geo, ".html"))
-carto <- "_site/modules/cartographie"
-list_files   <- c(list_files, list.files(carto, ".html"))
-repro <- "_site/modules/reproductibilite"
-list_files   <- c(list_files, list.files(repro, ".html"))
-
-
-file.copy(list.of.files, new.folder)
+# library(quarto)
+# pth <- getwd()
+# quarto_render(paste0(pth,"/modules/import/import.qmd"), output_file = "import.pdf", output_format = "pdf")
+# # quarto_render(paste0(pth,"/modules/graphique/Rbase_graph.qmd"), output_file = "Rbase_graph.pdf", output_format = "pdf")
+# quarto_render(paste0(pth,"/modules/reproductibilite/minicran.qmd"), output_file = "miniCRAN.pdf", output_format = "pdf")
