22394d62aba9535b75e1d80f782c5b4c83f5424d
HuguesPecout <hugues.pecout@cnrs.fr>
2023-02-21 23:39:35 +0100

    update

--- correction/OpenStreetMap/correction_exercice.R
+++ correction/OpenStreetMap/correction_exercice.R
@@ -6,16 +6,83 @@
 ##------------------ PACKAGES ----------------------##
 
 # install.packages("tidygeocoder")
+# install.packages("sf")
 # install.packages("mapview")
 # install.packages("osmextract")
 # install.packages("osrm")
 
 
-##------------------- IMPORT -----------------------##
+##------------------- Géocodage adresse -----------------------##
+
+
+"Institut Régional de Santé Publique de Ouidah, Route des Esclaves, 01 BP 918 Cotonou, Bénin"
+
+library(tidygeocoder)
+Univ_benin <- data.frame(name = c("IRSP", "LARESPD"),
+                         addresse = c("Institut Régional de Santé Publique de Ouidah, Bénin",
+                                      "Université de Parakou, Bénin"))
+
+
+Univ_benin_loc <- geocode(Univ_benin, addresse)
+Univ_benin_loc
+
+library(sf)
+Univ_benin_sf <- st_as_sf(Univ_benin_loc, coords = c("long", "lat"), crs = 4326)
+Univ_benin_sf
+
+library(mapview)
+mapview(Univ_benin_sf)
+
+Univ_benin_sf <- st_transform(Univ_benin_sf, 3857)
+
+library(maptiles)
+osm_tiles <- get_tiles(x = st_buffer(Univ_benin_sf, 80000), zoom = 8, crop = TRUE)
+
+
+plot_tiles(osm_tiles)
+plot(st_geometry(Univ_benin_sf), border = "red", col="red" , lwd = 10, pch = 20, add = TRUE)
+mtext(side = 1, line = -1, text = get_credit("OpenStreetMap"), col="tomato")
+
 
 library(sf)
 africapolis <- st_read("data/AFRICAPOLIS_2020/africapolis_extract.shp",  quiet = TRUE)
 
+africapolis <- st_transform(africapolis, 3857)
+
+africapolis_ben <- africapolis[africapolis$ISO3 == "BEN", ]
+africapolis_ben_pt <- st_centroid(africapolis_ben)
+
+
+mapview(africapolis_ben_pt)
+
+
+
+mat_dist_eucli <- st_distance(x = Univ_benin_sf, y = africapolis_ben_pt)
+class(mat_dist_eucli)
+
+
+africapolis_ben_pt$dist_IRSP <- mat_dist_eucli[1,]
+africapolis_ben_pt$dist_LARESPD <- mat_dist_eucli[2,]
+mean(africapolis_ben_pt$dist_IRSP)
+mean(africapolis_ben_pt$dist_LARESPD)
+
+library(mapsf)
+
+mf_map(x = africapolis_ben_pt,
+       var = "dist_LARESPD",
+       symbol = "circle",
+       type = "prop",
+       inches = 0.40,
+       col = "brown4",
+       border = "white",
+       lwd = 0.5,
+       leg_pos = "bottomleft2",
+       leg_title = "Nombre d'habitants (en millions)", 
+       add = TRUE)
+# mat_dist_eucli <- units::drop_units(mat_dist_eucli)
+
+row.names(mat_dist_eucli) <- Univ_benin_sf$name
+colnames(mat_dist_eucli, do.NULL =FALSE)<- africapolis$agglosName
 
 
 1. adresse ouidah - Création table
